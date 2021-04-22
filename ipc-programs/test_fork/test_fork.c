832a913cf65d40ef5a12872600925405032d741b
Valentin Lukyanets <valikluks95@gmail.com>
2021-04-23 01:28:15 +0300

    Changed fork() result checking to switch-case
    Signed-off-by: Valentin Lukyanets <valikluks95@gmail.com>

--- ipc-programs/test_fork/test_fork.c
+++ ipc-programs/test_fork/test_fork.c
@@ -6,12 +6,16 @@ int main()
 {
     pid_t pid = fork();
 
-    if (pid < 0) {
+    switch (pid) {
+    case -1:
         printf("Fork failed!\n");
-    } else if (pid == 0) {
+        break;
+    case 0:
         printf("Hello from child process, PID %d (parent PID %d)\n", getpid(), getppid());
-    } else {
+        break;
+    default:
         printf("Hello from parent process, PID %d\n", getpid());
+        break;
     }
 
     usleep(1000);
